cd689ba36168b322498799908653249500138124
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-02-05 13:55:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Previous Smaller Element/previous-smaller-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static ArrayList<Integer> prevSmaller(int[] arr) {
+        // code here
+        ArrayList<Integer>res=new ArrayList<>();
+        Stack<Integer>s=new Stack<>();
+        for(int i=0;i<arr.length;i++){
+            while(!s.isEmpty() && s.peek()>=arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty())res.add(-1);
+            else res.add(s.peek());
+            s.push(arr[i]);
+        }
+        return res;
+    }
+}

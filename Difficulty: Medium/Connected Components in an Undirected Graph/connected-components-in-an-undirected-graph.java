daef4d1a3ca189de7698b18c3532548ab8ef8771
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-02-25 11:02:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Connected Components in an Undirected Graph/connected-components-in-an-undirected-graph.java
@@ -0,0 +1,43 @@
+class Solution {
+    
+    
+    
+    public ArrayList<ArrayList<Integer>> getComponents(int V, int[][] edges) {
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
+        int v[]=new int[V];
+        Arrays.fill(v,0);
+        for(int i=0;i<V;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<edges.length;i++){
+            int x=edges[i][0];
+            int y=edges[i][1];
+            adj.get(x).add(y);
+            adj.get(y).add(x);
+        }
+        ArrayList<Integer>temp;
+        Queue<Integer>q=new LinkedList<>();
+       
+        for(int i=0;i<V;i++){
+            if(v[i]==0){
+                temp=new ArrayList<>();
+                q.offer(i);
+                v[i]=1;
+                while(!q.isEmpty()){
+                    int val=q.poll();
+                    temp.add(val);
+                    for(int x:adj.get(val)){
+                        if(v[x]==0){
+                        q.add(x);
+                        v[x]=1;
+                        }
+                    }
+                }
+                 ans.add(temp);
+            }
+           
+        }
+        return ans;
+    }
+}

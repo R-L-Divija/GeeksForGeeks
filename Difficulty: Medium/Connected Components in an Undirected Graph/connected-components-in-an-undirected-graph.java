aef309d54a065a53ca5674b840b31933d3f0eb91
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-02-26 14:55:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Connected Components in an Undirected Graph/connected-components-in-an-undirected-graph.java
+++ Difficulty: Medium/Connected Components in an Undirected Graph/connected-components-in-an-undirected-graph.java
@@ -1,10 +1,18 @@
 class Solution {
     
     
-    
+    public void func(int index, ArrayList<ArrayList<Integer>> adj,int v[],ArrayList<Integer>temp){
+        v[index]=1;
+        temp.add(index);
+        for(int x:adj.get(index)){
+            if(v[x]==0){
+                func(x,adj,v,temp);
+            }
+        }
+    }
     public ArrayList<ArrayList<Integer>> getComponents(int V, int[][] edges) {
         ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
-        ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
+       ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
         int v[]=new int[V];
         Arrays.fill(v,0);
         for(int i=0;i<V;i++){
@@ -16,25 +24,30 @@ class Solution {
             adj.get(x).add(y);
             adj.get(y).add(x);
         }
-        ArrayList<Integer>temp;
-        Queue<Integer>q=new LinkedList<>();
+        
+         ArrayList<Integer>temp;
+        // Queue<Integer>q=new LinkedList<>();
        
         for(int i=0;i<V;i++){
             if(v[i]==0){
-                temp=new ArrayList<>();
-                q.offer(i);
-                v[i]=1;
-                while(!q.isEmpty()){
-                    int val=q.poll();
-                    temp.add(val);
-                    for(int x:adj.get(val)){
-                        if(v[x]==0){
-                        q.add(x);
-                        v[x]=1;
-                        }
-                    }
-                }
-                 ans.add(temp);
+            //     temp=new ArrayList<>();
+            //     q.offer(i);
+            //     v[i]=1;
+            //     while(!q.isEmpty()){
+            //         int val=q.poll();
+            //         temp.add(val);
+            //         for(int x:adj.get(val)){
+            //             if(v[x]==0){
+            //             q.add(x);
+            //             v[x]=1;
+            //             }
+            //         }
+            //     }
+            //      ans.add(temp);
+            // 
+            temp=new ArrayList<>();
+            func(i,adj,v,temp);
+            ans.add(temp);
             }
            
         }

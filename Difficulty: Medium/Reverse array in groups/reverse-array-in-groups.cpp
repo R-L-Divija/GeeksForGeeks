92bd4aba162929ddaf2e68e0263f5dceda03057a
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-03-03 11:13:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Reverse array in groups/reverse-array-in-groups.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    void reverseInGroups(vector<int> &arr, int k) {
+        // code here
+          // code here
+        int n = arr.size();
+        for(int i=0;i<n;i=i+k)
+        {
+            int s = i;
+            int e = min(k+i-1,n-1);
+            while(s<e)
+            {
+                swap(arr[s++],arr[e--]);
+            }
+        }
+    }
+};

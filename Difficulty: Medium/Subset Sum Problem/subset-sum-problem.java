959b7a78c81d42a4f8890a620c05a6eb5802c010
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-03-06 14:24:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -1,33 +1,69 @@
 class Solution {
   
-    static boolean func(int index,int arr[],int s,int dp[][]){
+    // static boolean func(int index,int arr[],int s,int dp[][]){
         
-        if(index==arr.length){
-             if(s==0)return true;
-            return false;
-        }
-        if(s<0)return false;
-        //take
+    //     if(index==arr.length){
+    //          if(s==0)return true;
+    //         return false;
+    //     }
+    //     if(s<0)return false;
+    //     //take
         
-       if(dp[index][s]!=-1) return dp[index][s]==1;
-       boolean t1= func(index+1,arr,s-arr[index],dp);
+    //   if(dp[index][s]!=-1) return dp[index][s]==1;
+    //   boolean t1= func(index+1,arr,s-arr[index],dp);
       
-        boolean t2=func(index+1,arr,s,dp);
+    //     boolean t2=func(index+1,arr,s,dp);
         
-        dp[index][s]=(t1||t2)?1:0;
+    //     dp[index][s]=(t1||t2)?1:0;
         
-        return dp[index][s]==1;
-    }
+    //     return dp[index][s]==1;
+    // }
         
         
+    // static Boolean isSubsetSum(int arr[], int sum) {
+    //     // code here
+    //     int dp[][]=new int[arr.length][sum+1];
+    //     for(int i=0;i<arr.length;i++){
+    //     Arrays.fill(dp[i],-1);
+    //     }
+        
+    //     return func(0,arr,sum,dp);
+        
+    // }
     static Boolean isSubsetSum(int arr[], int sum) {
-        // code here
-        int dp[][]=new int[arr.length][sum+1];
-        for(int i=0;i<arr.length;i++){
-        Arrays.fill(dp[i],-1);
+        int n=arr.length;
+        boolean dp[][]=new boolean[n][sum+1];
+        //step 1
+        for(int i=0;i<n;i++){
+            dp[i][0]=true;
+        }
+        //step2
+        if(arr[n-1]<=sum){
+            dp[n-1][arr[n-1]]=true;
+        }
+        //step3
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<=sum;j++){
+                //not take
+                boolean nt=dp[i+1][j];
+                //take
+                boolean t=false;
+                if(j-arr[i]>=0){
+                    t=dp[i+1][j-arr[i]];
+                }
+                
+                dp[i][j]=(t||nt);
+            }
         }
         
-        return func(0,arr,sum,dp);
+        
+        
+        
+        return dp[0][sum];
+        
+        
+        
+        
         
     }
 }

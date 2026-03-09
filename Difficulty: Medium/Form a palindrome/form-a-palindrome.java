aa42ef97a3de99fa09ceb932edd66e814533cf5a
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-03-09 14:08:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Form a palindrome/form-a-palindrome.java
+++ Difficulty: Medium/Form a palindrome/form-a-palindrome.java
@@ -1,21 +1,25 @@
+// User function Template for Java
+
 class Solution {
-    int findMinInsertions(String s1) {
-        int n=s1.length();
-        int dp[][]=new int[n+1][n+1];
-        for(int i=0;i<=n;i++){
-            dp[i][0]=0;
-            dp[0][i]=0;
+    static int func(int i,int j,String s1,String s2,int dp[][]){
+        if(i==s1.length()||j==s2.length())return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(s1.charAt(i)==s2.charAt(j))return 1+func(i+1,j+1,s1,s2,dp);
+        else{
+            int m1=func(i,j+1,s1,s2,dp);
+            int m2=func(i+1,j,s1,s2,dp);
+            
+            return dp[i][j]= Math.max(m1,m2);
         }
-        String s2=new StringBuilder(s1).reverse().toString();
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(s1.charAt(i-1)==s2.charAt(j-1)){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }else{
-                    dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
-                }
-            }
-        }
-        return s1.length()-dp[n][n];
+    }
+    static int countMin(String s1) {
+      int n=s1.length();
+      int dp[][]=new int[n+1][n+1];
+      for(int i=0;i<=n;i++){
+          Arrays.fill(dp[i],-1);
+      }
+      String s2=new StringBuilder(s1).reverse().toString();
+      return s1.length()-func(0,0,s1,s2,dp);
+        
     }
 }

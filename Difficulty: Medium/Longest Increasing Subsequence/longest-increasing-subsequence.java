a55b75eb54ea0627ff16a2b937ea132cd0253106
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-03-10 11:22:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.java
+++ Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.java
@@ -1,23 +1,33 @@
 class Solution {
-    static int func(int i,int[] arr,int dp[]){
-        if(dp[i]!=-1)return dp[i];
-        int ans=1;
-        for(int j=i+1;j<arr.length;j++){
-            if(arr[i]<arr[j]){
-                ans=Math.max(ans,1+func(j,arr,dp));
-            }
-        }
-        return dp[i]=ans;
-    }
+    // static int func(int i,int[] arr,int dp[]){
+    //     if(dp[i]!=-1)return dp[i];
+    //     int ans=1;
+    //     for(int j=i+1;j<arr.length;j++){
+    //         if(arr[i]<arr[j]){
+    //             ans=Math.max(ans,1+func(j,arr,dp));
+    //         }
+    //     }
+    //     return dp[i]=ans;
+    // }
     static int lis(int arr[]) {
         
-    int n=arr.length;
-    int dp[]=new int[n+1];
-    Arrays.fill(dp,-1);
-    int ans=0;
-    for(int i=0;i<n;i++){
-     ans=Math.max(ans,func(i,arr,dp));
+    // int n=arr.length;
+    // int dp[]=new int[n+1];
+    // Arrays.fill(dp,-1);
+    // int ans=0;
+    // for(int i=0;i<n;i++){
+    //  ans=Math.max(ans,func(i,arr,dp));
+    // }
+    //   return ans; 
+    
+    
+    ArrayList<Integer>ans=new ArrayList<>();
+    for(int x:arr){
+        int i=Collections.binarySearch(ans,x);
+        if(i<0)i=-(i+1);
+        if(i==ans.size())ans.add(x);
+        else ans.set(i,x);
     }
-       return ans; 
+    return ans.size();
     }
 }

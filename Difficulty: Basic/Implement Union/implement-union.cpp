daa5fd723924fb86913b3e51b23bd85a8bbd188d
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-03-11 09:05:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Implement Union/implement-union.cpp
@@ -0,0 +1,21 @@
+// User function Template for C++
+
+/*create binary tree struct node*/
+// your code here
+struct NODE
+
+ {
+
+     //may2021
+
+     double data;
+
+     union{
+
+     struct NODE* left, *right;
+
+     };
+
+ };
+
+ 

c30ad928b9622ea859c5ea7cb1fb1a4a03fd1fda
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-03-11 14:39:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print Alphabets/print-alphabets.py
@@ -0,0 +1,6 @@
+#User function Template for python3
+def alphabets(c1,c2):
+     for i in range(ord(c1), ord(c2)+1):
+        print(chr(i), end=' ')
+    #Code below to print alphabets from c1 to c2
+    # Don't provide a new line after printing

7099d0db5f877ff7e94c29012b9d5a20447b3f28
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-03-12 09:43:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.java
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.java
@@ -1,33 +1,35 @@
 class Solution {
-    public boolean func(int a[],int k,int m){
-        int p=0;
+    public boolean func(int arr[],int k,int m){
         int s=1;
-        for(int x:a){
-            if(x>m)return false;
-            if(p+x<=m){
-              
-                p+=x;
-            }else{
-                  s++;
-                  p=x;
-            }
-        }
+        int p=0;
+       for(int x:arr){
+           if(x>m)return false;
+           if(x+p<=m)p+=x;
+           else {
+               s++;
+               p=x;
+           }
+       }
         return s<=k;
     }
     public int findPages(int[] arr, int k) {
-        if(k>arr.length)return-1;
-  int l=0;
-  int h=0;
-  for(int x:arr){
-      l=Math.max(l,x);
-      h+=x;
-  }
-  while(l<h){
-      int m=l+(h-l)/2;
-     if(func(arr,k,m)){
-         h=m;
-     }else l=m+1;
-  }
-   return l;
+        // code here
+        if(k>arr.length)return -1;
+        int l=0;
+        int h=0;
+        int ans=0;
+        for(int x:arr){
+            l=Math.max(l,x);
+            h+=x;
+        }
+        while(l<h){
+            int m=l+(h-l)/2;
+            if(func(arr,k,m)){
+                
+                h=m;
+            }
+            else l=m+1;
+            }
+            return l;
     }
 }

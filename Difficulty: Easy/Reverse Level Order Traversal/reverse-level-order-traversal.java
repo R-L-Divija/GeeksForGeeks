dab65cd1aa09e3f4473f255a70c6b45a5d9f4f6e
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-03-12 11:06:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse Level Order Traversal/reverse-level-order-traversal.java
@@ -0,0 +1,38 @@
+/*
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+
+class Tree {
+    public ArrayList<Integer> reverseLevelOrder(Node root) {
+     ArrayList<Integer>ans=new ArrayList<>();
+     ArrayList<Integer>temp;
+     Queue<Node>q=new LinkedList<>();
+     q.offer(root);
+     while(!q.isEmpty()){
+         int s=q.size();
+         temp=new ArrayList<>();
+         while(s-->0){
+             Node t=q.poll();
+             temp.add(t.data);
+             if(t.right!=null)q.offer(t.right);
+             if(t.left!=null)q.offer(t.left);
+         }
+         for(int x:temp){
+             ans.add(x);
+         }
+     }
+     Collections.reverse(ans);
+     return ans;
+        
+    }
+}

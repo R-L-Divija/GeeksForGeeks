9693d64c090c11058c5b31152490dd74bae93bba
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-03-12 10:43:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/ZigZag Tree Traversal/zig-zag-tree-traversal.java
+++ Difficulty: Medium/ZigZag Tree Traversal/zig-zag-tree-traversal.java
@@ -11,36 +11,29 @@ class Node {
 */
 
 class Solution {
-    
     ArrayList<Integer> zigZagTraversal(Node root) {
-        ArrayList<ArrayList<Integer>>ans=new ArrayList<>();
-        
-       Queue<Node>q=new LinkedList<>();
-       q.offer(root);
-       int c=1;
-       while(!q.isEmpty()){
-           int s=q.size();
-               ArrayList<Integer>temp=new ArrayList<>();
-           while(s-- >0){
-               
-               Node t=q.poll();
-               temp.add(t.data);
-               if(t.left!=null)q.offer(t.left);
-               if(t.right!=null)q.offer(t.right);
-           }
-           if(c%2!=0)ans.add(temp);
-           else{
-               Collections.reverse(temp);
-               ans.add(temp);
-           }
-           c++;
-       }
-       ArrayList<Integer>ans1=new ArrayList<>();
-       for(ArrayList<Integer> list:ans){
-       for(int x:list){
-           ans1.add(x);
-       }
-       }
-        return ans1;
+      ArrayList<Integer>ans=new ArrayList<>();
+     ArrayList<Integer>temp;
+     Queue<Node>q=new LinkedList<>();
+     q.offer(root);
+     int c=0;
+     while(!q.isEmpty()){
+         int s=q.size();
+         temp=new ArrayList<>();
+         c++;
+         while(s-->0){
+             Node t=q.poll();
+             temp.add(t.data);
+             if(t.left!=null)q.offer(t.left);
+             if(t.right!=null)q.offer(t.right);
+         }
+         if(c%2==0){
+             Collections.reverse(temp);
+         }
+         for(int x:temp){
+             ans.add(x);
+         }
+     }
+        return ans;
     }
 }

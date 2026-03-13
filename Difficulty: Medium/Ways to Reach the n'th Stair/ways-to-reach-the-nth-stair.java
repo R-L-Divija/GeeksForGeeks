61c5ce31c0dfd9aa9798432ab71aeac754ea5e9e
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-03-13 09:36:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.java
+++ Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.java
@@ -1,10 +1,16 @@
 class Solution {
-      int dp[]=new int[100];
+    //   int dp[]=new int[100];
     int countWays(int n) {
-      
-       if(n==0||n==1)return 1;
-       if(dp[n]!=0)return dp[n];
-       return dp[n]=countWays(n-1)+countWays(n-2);
+    //   if(n==0||n==1)return 1;
+    //   if(dp[n]!=0)return dp[n];
+    //   return dp[n]=countWays(n-1)+countWays(n-2);
         
+        int dp[]=new int[n+1];
+        dp[0]=1;
+        dp[1]=2;
+        for(int i=2;i<n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n-1];
     }
 }

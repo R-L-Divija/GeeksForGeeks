bcf8e62e9d665222babcaa4ae8d4c33d56388546
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-03-14 03:26:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -68,29 +68,26 @@ class Solution {
     // }
     
     
-    static boolean func(int index,int arr[],int s,int dp[][]){
-        if(index==arr.length){
-            if(s==0)return true;
-            return false;
-        }
-        if(s<0)return false;
-        if(dp[index][s]!=-1)return dp[index][s]==1;
-        //take
-        boolean t=func(index+1,arr,s-arr[index],dp);
-        //not take
-        boolean nt=func(index+1,arr,s,dp);
-        
-        dp[index][s]=(t||nt)?1:0;
-        return dp[index][s]==1;
+static boolean func(int i,int arr[],int s,int dp[][]){
+     if(s==0)return true;
+    if(i==arr.length){
+        if(s==0)return true;
+        return false;
     }
-    
+ 
+    if(s<0)return false;
+      if(dp[i][s]!=-1)return dp[i][s]==1;
+    boolean t=func(i+1,arr,s-arr[i],dp);
+    boolean nt=func(i+1,arr,s,dp);
+    dp[i][s]=(t||nt)?1:0;
+    return dp[i][s]==1;
+}
      static Boolean isSubsetSum(int arr[], int sum) {
-         int n=arr.length;
-        
-         int dp[][]=new int[n][sum+1];
-         for(int i=0;i<n;i++){
-             Arrays.fill(dp[i],-1);
-         }
-         return func(0,arr,sum,dp);
+       int n=arr.length;
+       int dp[][]=new int[arr.length][sum+1];
+       for(int i=0;i<arr.length;i++){
+           Arrays.fill(dp[i],-1);
+       }
+       return func(0,arr,sum,dp);
      }
 }

437628daee9672f3bcefca371af72b0f7bdd65bb
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-03-14 04:42:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Minimum number of Coins/minimum-number-of-coins.java
+++ Difficulty: Easy/Minimum number of Coins/minimum-number-of-coins.java
@@ -1,22 +1,24 @@
 class Solution {
     public int findMin(int n) {
-    
     int ans=0;
     int rem=0;
     
-     rem=n/10;
+    rem=n/10;
      n=n%10;
      ans+=rem;
      
-    rem=n/5;
+     rem=n/5;
      n=n%5;
      ans+=rem;
+     
      rem=n/2;
      n=n%2;
      ans+=rem;
+     
      rem=n/1;
      n=n%1;
      ans+=rem;
-      return ans;  
+     
+     return ans;  
     }
 }

b3b353ca8b6664132763db5a733188937f534ed2
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-03-14 13:26:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find if +ve, -ve or 0/find-if-ve-ve-or-0.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Take a as input
+
+    // find it is positive, negative or zero
+     int a;
+    cin>>a;
+    if(a==0)
+    {
+        cout<<"Zero";
+    }
+    if(a<0)
+    {
+        cout<<"Negative";
+    }
+    if(a>0)
+    {
+        cout<<"Positive";
+    }
+
+    return 0;
+    return 0;
+}

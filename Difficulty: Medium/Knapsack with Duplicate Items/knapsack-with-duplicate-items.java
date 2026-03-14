187f1512a59ee8a1b6dd9495dde50a77a1b11519
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-03-14 05:49:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.java
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.java
@@ -1,26 +1,22 @@
 class Solution {
-    public int func(int index, int val[],int wt[],int t,int dp[][]){
-        if(t==0) return 0;
-        if(t<0)return Integer.MIN_VALUE;
-        if(index==wt.length){
+    public int func(int i,int w[],int v[],int s,int dp[][]){
+       if(s==0)return 0;
+       if(s<0)return Integer.MIN_VALUE;
+       if(i==w.length){
             return 0;
         }
-        if(dp[index][t]!=-1)return dp[index][t];
-        //take
-        int m1=val[index]+func(index,val,wt,t-wt[index],dp);
-        //not take
-        int m2= func(index+1,val,wt,t,dp);
+        if(dp[i][s]!=-1)return dp[i][s];
+        int t=v[i]+func(i,w,v,s-w[i],dp);
         
-        return dp[index][t]=Math.max(m1,m2);
+        int nt=func(i+1,w,v,s,dp);
+        return dp[i][s]=Math.max(nt,t);
     }
-    public int knapSack(int val[], int wt[], int t) {
-        // code here
-        
-       int dp[][]=new int[val.length][t+1];
-       for(int i=0;i<val.length;i++){
-           Arrays.fill(dp[i],-1);
-       }
-        return func(0,val,wt,t,dp);
+    public int knapSack(int val[], int wt[], int capacity) {
+        int dp[][]=new int[wt.length][capacity+1];
+        for(int i=0;i<wt.length;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return func(0,wt,val,capacity,dp);
         
     }
 }

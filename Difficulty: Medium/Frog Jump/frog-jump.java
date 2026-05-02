52e0a15e3a7db303ca5d9a3d3f20f097a983f452
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-05-02 20:09:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.java
+++ Difficulty: Medium/Frog Jump/frog-jump.java
@@ -1,17 +1,17 @@
 class Solution {
     int minCost(int[] h) {
-        // code here
+
         int n=h.length;
         if(n==1)return 0;
         if(n==2)return Math.abs(h[0]-h[1]);
-        int dp[]=new int[h.length+1];
-        dp[0]=0;
-        dp[1]=Math.abs(h[0]-h[1]);
-        for(int i=2;i<n;i++){
-            int m1=Math.abs(h[i]-h[i-1])+dp[i-1];
-            int m2=Math.abs(h[i]-h[i-2])+dp[i-2];
-            dp[i]=Math.min(m1,m2);
-        }
+       int dp[]=new int[n+1];
+       dp[0]=0;
+       dp[1]=Math.abs(h[0]-h[1]);
+       for(int i=2;i<n;i++){
+           int m1=Math.abs(h[i-1]-h[i])+dp[i-1];
+           int m2=Math.abs(h[i]-h[i-2])+dp[i-2];
+           dp[i]=Math.min(m1,m2);
+       }
         return dp[n-1];
     }
 }

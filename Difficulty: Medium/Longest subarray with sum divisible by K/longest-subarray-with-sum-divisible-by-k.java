e24bf5baf299c2a87eb719f913366e64304bc542
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-06-12 10:35:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest subarray with sum divisible by K/longest-subarray-with-sum-divisible-by-k.java
+++ Difficulty: Medium/Longest subarray with sum divisible by K/longest-subarray-with-sum-divisible-by-k.java
@@ -2,20 +2,22 @@
 
 class Solution {
     int longestSubarrayDivK(int[] arr, int k) {
-      HashMap<Integer,Integer>map=new HashMap<>();
-      long s=0;
-      int max=0;
-      map.put(0,-1);
-      for(int i=0;i<arr.length;i++){
-          s+=arr[i];
-          int r=(int)(s%k);
-          if(r<0)r=r+k;
-          if(map.containsKey(r)){
-             max=Math.max(max,i-map.get(r));
-          }else {
-              map.put(r,i);
-          }
-      }
-      return max;
+ HashMap<Integer,Integer>map=new HashMap<>();
+ map.put(0,-1);
+ long s=0;
+ int max=0;
+ for(int i=0;i<arr.length;i++){
+     s=s+arr[i];
+     int r=(int)(s%k);
+     if(r<0)r+=k;
+     if(map.containsKey(r)){
+         int l=i-map.get(r);
+         max=Math.max(max,l);
+         
+     }else{
+         map.put(r,i);
+     }
+ }return max;
+ 
     }
 }

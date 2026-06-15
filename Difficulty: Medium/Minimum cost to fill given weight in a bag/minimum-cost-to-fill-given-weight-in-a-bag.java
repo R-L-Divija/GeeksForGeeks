465bbfdc797a97f36ab517557263f0d3a0bd56e2
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-06-15 09:43:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum cost to fill given weight in a bag/minimum-cost-to-fill-given-weight-in-a-bag.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumCost(int[] cost, int w) {
+        
+        int n=cost.length;
+          int dp[]=new int[w+1];
+          for(int i=0;i<=w;i++){
+              dp[i]=Integer.MAX_VALUE;
+          }
+          dp[0]=0;
+          for(int i=1;i<=n;i++){
+              if(cost[i-1]!=-1){
+                  for(int j=i;j<=w;j++){
+                      if(dp[j-i]!=Integer.MAX_VALUE){
+                      dp[j]=Math.min(dp[j],dp[j-i]+cost[i-1]);}
+                  }
+              }
+          }
+          return dp[w]==Integer.MAX_VALUE?-1:dp[w];
+        
+    }
+}

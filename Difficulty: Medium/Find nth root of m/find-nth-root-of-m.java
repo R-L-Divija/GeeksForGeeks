936f93995842e709ea9fcdee60750e330ea77724
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-06-20 12:01:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find nth root of m/find-nth-root-of-m.java
+++ Difficulty: Medium/Find nth root of m/find-nth-root-of-m.java
@@ -1,14 +1,31 @@
 class Solution {
-    public int nthRoot(int n, int m) {
-        // code here
-        if(m==0)return 0;
-        if(n==0)return 1;
-        if(n==1)return m;
-        int i=1;
-        while(i<=9){
-            if(Math.pow(i,n)==m)return i;
-            i++;
+    public long func(long a,int b,int limit){
+        long r=1;
+        while(b>0){
+            if((b&1)==1){
+                
+                r*=a;
+               if(r>limit)return limit+1; 
+            }
+                
+            a*=a;
+            if(a>limit)a= limit+1; 
+            b>>=1;
+        }
+        return r;
+    }
+    public int nthRoot(int n, int n2) {
+        if(n2==0)return 0;
+        int l=1;
+        int r=n2;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            long pow=func(m,n,n2);
+            if(pow==n2)return m;
+            else if(pow< n2)l=m+1;
+            else r=m-1;
         }
         return -1;
+        
     }
 }

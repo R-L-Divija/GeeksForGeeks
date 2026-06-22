e87ff91de44caa4cdf30b50ce39711c10feceecc
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-06-22 12:18:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.java
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.java
@@ -1,29 +1,27 @@
 class Solution {
-    public boolean func(int a[],int k, int m){
+    public boolean func(int s[],int k,int m){
         int c=1;
-        int l=a[0];
-        for(int i=1;i<a.length;i++){
-            if(a[i]-l>=m){
+        int prev=s[0];
+        for(int i=1;i<s.length;i++){
+            if(s[i]-prev>=m){
                 c++;
-                l=a[i];
+                prev=s[i];
             }
         }
         return c>=k;
     }
-    public int aggressiveCows(int[]a, int k) {
-        Arrays.sort(a);
-        int ans=0;
-        int l=1;
-        int h=a[a.length-1]-a[0];
-        while(l<=h){
-            int mid=l+(h-l)/2;
-            if(func(a,k,mid)){
-                ans=mid;
-                l=mid+1;
-            }else{
-                h=mid-1;
-            }
-        }
-        return ans;
+    public int aggressiveCows(int[] s, int k) {
+        Arrays.sort(s);
+       int l=1;
+       int r=s[s.length-1]-s[0];
+       int a=0;
+       while(l<=r){
+         int m=l+(r-l)/2;
+         if(func(s,k,m)){
+             a=m;
+             l=m+1;
+         }else r=m-1;
+       }
+        return a;
     }
 }

7a8176b94a2e8b58cc59c1c6583a61b21dfaaaef
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-08-14 15:31:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -1,25 +1,14 @@
 class Solution {
-
-
-    public static int solve(int i,int s,int arr[],int t,int dp[][]){
-        if(i==arr.length){
-            return (s==t)? 1:0;
-        }
-        if(dp[i][s]!=-1)return dp[i][s];
-        int ta=0;
-        if(s+arr[i]<=t){
-            ta=solve(i+1,s+arr[i],arr,t,dp);
-        }
-        int nt=solve(i+1,s,arr,t,dp);
-        return dp[i][s]=ta+nt;
-    }
-    static Boolean isSubsetSum(int arr[], int sum) {
-        int dp[][]=new int[arr.length][sum+1];
-        for(int i=0;i<arr.length;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        int x=solve(0,0,arr,sum,dp);
-        return x!=0?true:false;
+    static boolean func(int arr[],int s,int index,int t){
+        if(s==t)return true;
+        if(s>t ||index==arr.length)return false;
+        if(func(arr,s+arr[index],index+1,t))return true;
+        if(func(arr,s,index+1,t))return true;
+        return false;
         
+    }
+    static boolean isSubsetSum(int arr[], int sum) {
+        // code here
+        return func(arr,0,0,sum);
     }
 }
